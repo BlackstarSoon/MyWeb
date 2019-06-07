1e661aec099cdb15cf87d9935e964083289f7a31
Wei Liang <weiliang317@outlook.com>
2019-06-07 17:04:16 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
 <html>
 <head>
 	<title>My Web Application</title>
+	<link href="plugins/bootstrap-4.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<script src="plugins/bootstrap-4.3.1/js/modernizr.min.js"></script>
 </head>
 <body>
-	<h1>Testing</h1>
+	<div class="container">
+		<h1>Testing</h1>
+	</div>
 </body>
 </html>
